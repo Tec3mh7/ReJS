7f4732499224895128cd1a380e1aaebc6132d253
MykhailoH-dev <mihailgavriluk@gmail.com>
2025-03-31 07:28:47 -0700

    Learned some variable features

---
+++ js/index.js
@@ -0,0 +1,27 @@
+// console.log(`hello`);
+// console.log(`i like pizza`);
+
+// window.alert(`This is an alert`)
+// window.alert(`i like pizza`)
+
+// this is a comment
+
+// document.getElementById("Myh1").textContent = "hello";
+// document.getElementById(`Myp`).textContent = `user`;
+// let age = 25; 
+// console.log(`You are ${age} years old`)
+
+// let FirstName = "Bro";
+
+// console.log(typeof FirstName)
+
+// let online = true;
+// console.log(typeof online);
+
+let fullName = "Bro code";
+let age = 16;
+let isStudent = true;
+
+document.getElementById("p1").textContent = `Full name: ${fullName}`;
+document.getElementById("p2").textContent = `Age: ${age}`;
+document.getElementById("p3").textContent = `Is student: ${isStudent}`
